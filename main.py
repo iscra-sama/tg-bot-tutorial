a3a05417d9c80c70fb012714694381436474ba2e
iscra-sama <nyaascorka@gmail.com>
2026-03-14 14:28:09 +0000

    upd

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from os import getenv
-from dotenv import load_dotenv
 import asyncio
+from os import getenv
+
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
 
-from iscrohandlers.routes import router
-
-load_dotenv()
-BOT_TOKEN = getenv("BOT_TOKEN")
+load_dotenv(dotenv_path=".env")
+bot = Bot(token=getenv("BOT_TOKEN"))
 dp = Dispatcher()
-dp.include_router(router)
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("I am born.")
+
+@dp.message(Command("iscroecho"))
+async def echo(message: Message):
+    entries = (message.text).split(maxsplit=1)
+    if len(entries) == 0:
+        return await message.answer(f"I'm a bot that echos: you sent nothing.")
+    await message.answer(f"I'm a bot that echos: «{entries[1]}».")
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    print("Bot started")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        print(e)
+        exit(0)
