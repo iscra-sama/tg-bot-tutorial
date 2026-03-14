9c92a16e084fa89f5abcd3a89a3378b7a81c80cc
iscra-sama <nyaascorka@gmail.com>
2026-03-14 09:16:44 +0000

    upd

--- iscrohandlers/routes.py
+++ iscrohandlers/routes.py
@@ -3,11 +3,12 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 router = Router()
+
 @router.message(Command('start'))
-async def hello(message: Message):
+async def start(message: Message):
     await message.answer("Hello, Iscra-chan.")
+
 @router.message(Command('iscroecho'))
-async def hello(message: Message):
-    entry = (message.text).find("")
-    print(entry)
-    await message.answer(f"Ви напейсали: «{entry}».")
+async def iscroecho(message: Message):
+    print(message.text)
+    return await message.answer(f"Ви напейсали: «dbg».")
