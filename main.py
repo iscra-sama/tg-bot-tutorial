52720c433b218981080af13e3af3534e0198adeb
iscra-sama <nyaascorka@gmail.com>
2026-03-14 15:17:35 +0000

    upd

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 from os import getenv
+from pprint import pprint
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
@@ -16,10 +17,24 @@ async def start(message: Message):
 
 @dp.message(Command("iscroecho"))
 async def echo(message: Message):
-    entries = (message.text).split(maxsplit=1)
-    if len(entries) == 0:
+    entry_maybe = (message.text).split(maxsplit=1)
+    if len(entry_maybe) == 0:
         return await message.answer(f"I'm a bot that echos: you sent nothing.")
-    await message.answer(f"I'm a bot that echos: «{entries[1]}».")
+    await message.answer(f"I'm a bot that echos: «{entry_maybe[1]}».")
+
+@dp.message(F.text == "ping")
+async def ping(message: Message):
+    await message.answer("pong!")
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    orig_photo = message.photo[-1]
+    pprint(orig_photo, indent=4)
+    async def _():
+        await message.answer_photo(photo=orig_photo.file_id, caption="Photo.")
+    async def __():
+        await message.answer("Photo.")
+    await asyncio.gather(_(), __())
 
 async def main():
     await dp.start_polling(bot)
